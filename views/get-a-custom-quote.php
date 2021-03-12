c88b1d2853a75ebbe333aa38ae89509f27967166
akashrajput9 <akashrajput9@hotmail.com>
2021-03-12 17:07:47 +0500

    udapte

--- views/get-a-custom-quote.php
+++ views/get-a-custom-quote.php
@@ -80,7 +80,12 @@
                 <p class="subtitle">Audit Your Site - More Sales, More Leads, More Happy Customers... </p>
                 <div class="col-lg-8 offset-lg-2">
                   <div class="bot-form justpad">
-                    <form class="cmxform" id="contactForm"  method="POST" action="https://logovalley.co.uk/webpages/packageFormController.php">
+                    <form class="cmxform" id="contactForm"  method="POST" action="/leads/">
+                    <input type="hidden" name="route" value="/order/?pack=4">   
+                        <input type="hidden" name="brand" value="logovalley">   
+                        <input type="hidden" name="tag" value="order page">
+                        <input type="hidden" name="price" value="null">
+                        <input type="hidden" name="news" value="1">
                       <div class="row">
                         <div class="col-lg-12">
                         <div class="bot-formtop">
@@ -88,110 +93,26 @@
                           <div class="fldset">
                             <div class="row">
                                 <div class="col-lg-6">
-                                    <input onkeypress="return ((event.charCode >= 65 && event.charCode <= 90) || (event.charCode >= 97 && event.charCode <= 122) || (event.charCode == 32))" id="username" name="Name" minlength="2" type="text" placeholder="Enter your name" required />
+                                    <input onkeypress="return ((event.charCode >= 65 && event.charCode <= 90) || (event.charCode >= 97 && event.charCode <= 122) || (event.charCode == 32))" id="username" name="name" minlength="2" type="text" placeholder="Enter your name" required />
                                 </div>
                                 <div class="col-lg-6">
-                                    <input id="cemail" type="email" name="Email" placeholder="Enter email here" required>
+                                    <input id="cemail" type="email" name="email" placeholder="Enter email here" required>
                                 </div>
                             </div>
                           </div>
                         
                           <div class="fldset">
-                            <input id="phone-coun" maxlength="10" name="Number" type="number" placeholder="Phone Number" required />
+                            <input id="phone-coun" maxlength="10" name="phone" type="number" placeholder="Phone Number" required />
                           </div>
                         
                           <div class="fldset">
-                            <select name="Services" id="selectcat" class="valid" aria-invalid="false" >
-                              <option  value="" pack="0">Package Name</option>
-                              <optgroup>
-                                <option disabled value="" pack="0" class="colr">Logo Design</option>
-                              </optgroup>
-                              <optgroup label="&nbsp;&nbsp;&nbsp;&nbsp;--------------------------------------------">
-                                <option  value="Start Up Logo Package - £15.99" pack="1" >Start Up Logo Package - £15.99</option>
-                                <option  value="Professional Logo Package - £24.99" pack="2" >Professional Logo Package - £24.99</option>
-                                <option  value="Elite Logo Package - £48.99" pack="3" >Elite Logo Package - £48.99</option>
-                                <option  value="Business Logo Package - £99.99" pack="4" >Business Logo Package - £129.99</option>
-                                <option  value="Silver Logo Package - £119.99" pack="5" >Business Rebranding Logo Package - £199.99</option>
-                                <!--<option  value="Gold Logo Package - £199.99" pack="6" >Gold Logo Package - £199.99</option>-->
-                                <!--<option  value="Illustrative  Logo Package - £109.99" pack="7" >Illustrative  Logo Package - £109.99</option>-->
-                                <option  value="3D Logo Package - £299.99" pack="8" >3D Logo Package - £299.99</option>
-                              </optgroup>
-
-                              <optgroup>
-                                <option disabled value="" pack="0" class="colr">Corporate Branding</option>
-                              </optgroup>
-                              <optgroup label="&nbsp;&nbsp;&nbsp;&nbsp;--------------------------------------------">
-                              <option  value="Startup Collateral Packages - £99.00" pack="9" >Startup Collateral Packages - £99.00</option>
-                              <option  value="Classic Collateral Packages - £199.00" pack="10" >Classic Collateral Packages - £199.00</option>
-                              <option  value="Premium Collateral Packages - £399.00" pack="11" >Premium Collateral Packages - £399.00</option>
-                              </optgroup>
-                              <option  value="Unlimited Collateral Packages - £499.00" pack="38" >Unlimited Collateral Packages - £499.00</option>
-                              </optgroup>
-
-                              <optgroup>
-                                <option disabled value="" pack="0" class="colr">Logo Animation</option>
-                              </optgroup>
-                              <optgroup label="&nbsp;&nbsp;&nbsp;&nbsp;--------------------------------------------">
-                              <option  value="2D Standard Logo Animation - £149.00" pack="12" >2D Standard Logo Animation - £149.00</option>
-                              <option  value="2D Advance Logo Animation Packages - £199.00" pack="13" >2D Advance Logo Animation Packages - £199.00</option>
-                              <option  value="3D Standard Logo Animation Packages - £244.00" pack="14" >3D Standard Logo Animation Packages - £244.00</option>
-                              <option  value="3D Advance Logo Animation - £299.00" pack="15" >3D Advance Logo Animation - £299.00</option>
-                              </optgroup>
-
-                              <optgroup>
-                                <option disabled value="" pack="0" class="colr">2D/3D Illustration</option>
-                              </optgroup>
-                              <optgroup label="&nbsp;&nbsp;&nbsp;&nbsp;--------------------------------------------">
-                              <option  value="Basic 2D illustration Packages - £195.00" pack="16" >Basic 2D illustration Packages - £195.00</option>
-                              <option  value="Standard 2D Illustration - £399.00" pack="17" >Standard 2D Illustration - £399.00</option>
-                              <option  value="Advance 2D Illustration - £599.00" pack="18" >Advance 2D Illustration - £599.00</option>
-                              <option  value="Advance Pro 2D Illustration - £799.00" pack="19" >Advance Pro 2D Illustration - £799.00</option>
-                              <option  value="Premium 2D Illustration - £999.00" pack="20" >Premium 2D Illustration - £999.00</option>
-                              </optgroup>
-
-                              <optgroup>
-                                <option disabled value="" pack="0" class="colr"> Content Writing Packages</option>
-                              </optgroup>
-                              <optgroup label="&nbsp;&nbsp;&nbsp;&nbsp;--------------------------------------------">
-                              <option  value="Web Content Packages - 70.00" pack="21" >Web Content Packages - 70.00</option>
-                              <option  value="Article Writing Packages - £40.00" pack="22" >Article Writing Packages - £40.00</option>
-                              <option  value="Creative Writing Packages - £75.00" pack="23" >Creative Writing Packages - £75.00</option>
-                              <option  value="Blog Writing Packages - £40.00" pack="24" >Blog Writing Packages - £40.00</option>
-                              <option  value="Infographics Packages - £200.00" pack="25" >Infographics Packages - £200.00</option>
-                              </optgroup>
-
-                              <optgroup>
-                                <option disabled value="" pack="0" class="colr"> Web Design Packages</option>
-                              </optgroup>
-                              <optgroup label="&nbsp;&nbsp;&nbsp;&nbsp;--------------------------------------------">
-                              <option  value="Basic Website Packages - £244.00" pack="26" >Basic Website Packages - £244.00</option>
-                              <option  value="Startup Website Packages - £394.00" pack="27" >Startup Website Packages - £394.00</option>
-                              <option  value="Professional Website Packages - £844.00" pack="28" >Professional Website Packages - £844.00</option>
-                              <option  value="Elite Website Packages - £1,494.00" pack="29" >Elite Website Packages - £1,494.00</option>
-                              <option  value="Corporate Website Packages - £2,394.00" pack="30" >Corporate Website Packages - £2,394.00</option>
-                              <option  value="Business Website Packages - £2,999.00" pack="31" >Business Website Packages - £2,999.00</option>
-                              </optgroup>
-
-                              <optgroup>
-                                <option disabled value="" pack="0" class="colr"> Combo Packages</option>
-                              </optgroup>
-                              <optgroup label="&nbsp;&nbsp;&nbsp;&nbsp;--------------------------------------------">
-                              <option  value="Basic Combo Package - £499.00" pack="32" >Basic Combo Package - £499.00</option>
-                              <option  value="Startup Combo Package - £799.00" pack="33" >Startup Combo Package - £799.00</option>
-                              <option  value="Professional Combo package - £1,199.00" pack="34" >Professional Combo package - £1,199.00</option>
-                              <option  value="Corporate Combo Package - £1,599.00" pack="35" >Corporate Combo Package - £1,599.00</option>
-                              <option  value="Elite Combo package - £2,999.00" pack="36" >Elite Combo package - £2,999.00</option>
-                              <option  value="Advance Combo Packages - £4,994.00" pack="37" >ADVANCE COMBO Packages - £4,994.00</option>
-                              </optgroup>
-
-
-                            </select>
+                          
                           </div>
                         </div>
                       </div>    
                         <div class="col-lg-12">
                           <div class="fldset">
-                            <textarea name="Message" placeholder="Enter message here" /></textarea>
+                            <textarea name="brief" placeholder="Enter message here" /></textarea>
                           </div>
                         </div>
                         <div class="col-lg-12">
